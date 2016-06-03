33dd55ecba1773e29243c2937d97c760baecf37c
Pavlo Sumkin <psumkin@mirantis.com>
2016-06-03 18:24:48 +0300

    [ui] Drop atom-editorconfig
    The atom-editorconfig seems too buggy

--- ui/app/components/home/home.ts
+++ ui/app/components/home/home.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-home',
-    templateUrl: 'app/components/home/home.html',
+  selector: 'my-home',
+  templateUrl: 'app/components/home/home.html',
 })
 
 export class HomeComponent {
