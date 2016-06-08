0c69338d7e15e50e51bc930c04a594da298b84d3
Oleksandr Minakov <aminakov@mirantis.com>
2016-06-08 14:35:38 +0300

    Add command for running blueprint

---
+++ cmd/nshore/app/blueprint.go
@@ -0,0 +1,37 @@
+// Copyright 2016 The NorthShore Authors All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var path string
+var blueprintCmd = &cobra.Command{
+	Use:   "blueprint",
+	Short: "Run execution of blueprint",
+	Long:  `This command read, parse and process blueprint.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Blueprint was runned.")
+		fmt.Printf("PATH -> %s \n", path)
+	},
+}
+
+func init() {
+	blueprintCmd.Flags().StringVarP(&path, "file", "f", ".", "Path to blueprint yaml")
+	runCmd.AddCommand(blueprintCmd)
+}
