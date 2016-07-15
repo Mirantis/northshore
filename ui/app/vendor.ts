cfa9f2e0f85f3960ee088201e68e42ef78ec9a25
Oleksandr Minakov <aminakov@mirantis.com>
2016-07-15 16:32:24 +0300

    Add bootstrap and font-awesome

--- ui/app/vendor.ts
+++ ui/app/vendor.ts
@@ -11,9 +11,9 @@ import '@angular/router';
 // RxJS
 import 'rxjs';
 
-//import 'bootstrap'
-//import 'font-awesome'
-//import 'ng2-bootstrap'
+import 'bootstrap'
+import 'font-awesome'
+import 'ng2-bootstrap'
 import 'core-js/client/shim'
 import 'zone.js/dist/zone'
 import 'reflect-metadata'
