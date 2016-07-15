23a91b325f7ff34387315f49a09eab222c67e591
Oleksandr Minakov <aminakov@mirantis.com>
2016-07-15 16:19:35 +0300

    Add vendors

---
+++ ui/app/vendor.ts
@@ -0,0 +1,20 @@
+// Vendors
+
+// Angular 2
+import '@angular/platform-browser-dynamic';
+import '@angular/platform-browser';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router';
+
+// RxJS
+import 'rxjs';
+
+//import 'bootstrap'
+//import 'font-awesome'
+//import 'ng2-bootstrap'
+import 'core-js/client/shim'
+import 'zone.js/dist/zone'
+import 'reflect-metadata'
+import 'object-assign'
